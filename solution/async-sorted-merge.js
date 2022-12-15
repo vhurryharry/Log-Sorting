db5696e6f182c9601b036d6dd3b07ddb2ef38317
Alexander Bai <vhurryharry@gmail.com>
2022-12-15 08:38:48 -0600

    add sync/async sortings

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,36 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+  let entries = await Promise.all(logSources.map(async (ls, index) => {
+    const entry = await ls.pop()
+    return {
+      index,
+      entry,
+    }
+  }))
+  entries = entries
+    .filter(log => log.entry !== false)
+    .sort((a, b) => a.entry.date > b.entry.date ? 1 : -1)
+
+  while (entries.length > 0) {
+    const entry = entries.shift()
+    printer.print(entry.entry)
+
+    const newEntry = await logSources[entry.index].pop()
+    if (newEntry) {
+      let i
+      for (i = 0; i < entries.length; i++) {
+        if (newEntry.date < entries[i].entry.date) break
+      }
+
+      entries.splice(i, 0, {
+        index: entry.index,
+        entry: newEntry,
+      })
+    }
+  }
+  printer.done()
+
+  return console.log("Async sort complete.");
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,31 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  let entries = logSources.map((ls, index) => ({
+    index,
+    entry: ls.pop(),
+  }))
+    .filter(log => log.entry !== false)
+    .sort((a, b) => a.entry.date > b.entry.date ? 1 : -1)
+
+  while (entries.length > 0) {
+    const entry = entries.shift()
+    printer.print(entry.entry)
+
+    const newEntry = logSources[entry.index].pop()
+    if (newEntry) {
+      let i
+      for (i = 0; i < entries.length; i++) {
+        if (newEntry.date < entries[i].entry.date) break
+      }
+
+      entries.splice(i, 0, {
+        index: entry.index,
+        entry: newEntry,
+      })
+    }
+  }
+  printer.done()
+
+  return console.log("Sync sort complete.")
 };
